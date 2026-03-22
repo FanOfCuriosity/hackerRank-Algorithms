09ca52c9d2a76d9cb39c0402b76eed9e62c8184c
0xB4PH0 <rcleito129@gmail.com>
2026-03-22 19:38:12 -0300

    solution of implementation/day-of-programmer.py

---
+++ implementation/day-of-the-programmer.py
@@ -0,0 +1,14 @@
+def dayOfProgrammer(year: int):
+    if 1700 <= year <= 1917:
+        if year % 4 == 0:
+            return f"12.09.{year}"
+        else:
+            return f"13.09.{year}"
+
+    elif year >= 1919:
+        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+            return f"12.09.{year}"
+        else:
+            return f"13.09.{year}"
+    elif year == 1918:
+        return f"26.09.{year}"
